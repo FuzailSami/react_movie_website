0a41d7b7ebf1b3738f75548ba0a36c17e4699b29
Fuzail Sami <samifuzail@gmail.com>
2025-06-09 17:41:32 -0500

    commit3

---
+++ react_project/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY ="70a7b301f34a6b825e97d9da7e3f677f";
+const BASE_URL ="https://www.themoviedb.org";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
